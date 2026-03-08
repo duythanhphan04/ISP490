8abd6094c55c0570923cf5d7292b26177aee2543
MnnM <duyptse184270@fpt.edu.vn>
2026-03-08 10:34:31 +0700

    UserGroupRepository change quetry

--- identity-service/src/main/java/com/devteria/identity_service/repository/UserGroupRepository.java
+++ identity-service/src/main/java/com/devteria/identity_service/repository/UserGroupRepository.java
@@ -11,9 +11,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserGroupRepository extends JpaRepository<UserGroup, String> {
-    @Query("SELECT ug.user FROM UserGroup ug WHERE ug.group.group_id = :groupId")
+    @Query("SELECT ug.user FROM UserGroup ug WHERE ug.group.group_id = :groupId AND ug.status = 'INACTIVE'")
     List<User> findAllUsersByGroupId(@Param("groupId") String groupId);
-    @Query("SELECT ug.group FROM UserGroup ug WHERE ug.user.user_id = :userId")
+    @Query("SELECT ug.group FROM UserGroup ug WHERE ug.user.user_id = :userId AND ug.status = 'INACTIVE'")
     List<Group> findAllGroupsByUserId(@Param("userId") String userId);
     @Query("SELECT ug FROM UserGroup ug WHERE ug.user.user_id = :userId AND ug.group.group_id = :groupId")
     Optional<UserGroup> findByUserIdAndGroupId(@Param("userId") String userId, @Param("groupId") String groupId);
